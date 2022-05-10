0c905e306ed86bad216e4a076ab23e24477c1fcd
Bhanu Prasanth Konda <32713350+bhanuprasanthkonda@users.noreply.github.com>
2022-05-10 03:16:33 -0700

    Added embedment percentqge

--- main.py
+++ main.py
@@ -6,22 +6,27 @@ import os
 sys.setrecursionlimit(1000000)
 
 
+# reads the image from the filepath
 def read_image(filepath):
     return cv2.imread(filepath)
 
 
+# show the image
 def show_image(window_name, image):
     cv2.imshow(window_name, image)
 
 
+# returns a converted image
 def image_conversion(image, formate):
     return cv2.cvtColor(image, formate)
 
 
+# returns a new empty image
 def creating_empty_image(dim, dtype):
     return np.zeros(dim, dtype)
 
 
+# setting the contras and brightness of the image
 def image_contrast_brightness(image):
     new_image = creating_empty_image(image.shape, image.dtype)
     alpha = 4
@@ -32,6 +37,7 @@ def image_contrast_brightness(image):
     return new_image
 
 
+# scaling the image to (350,) or (0,2000) dimensions
 def image_scaling(image):
     scale_percent = min(35000 / image.shape[0], 200000 / image.shape[1])
     width = int(image.shape[1] * scale_percent / 100)
@@ -40,6 +46,7 @@ def image_scaling(image):
     return cv2.resize(image, dim, interpolation=cv2.INTER_AREA)
 
 
+# sharpening the image
 def image_sharpness(image):
     kernel = np.array([[0, -1, 0],
                        [-1, 5, -1],
@@ -48,53 +55,88 @@ def image_sharpness(image):
     return image_sharp
 
 
-def print_stone(image, i, j, new_image):
+# writing the selected stone to a new_image
+def print_stone(image, i, j, new_image, boarder, min_max):
     max_j = j
     if image[i][j] > 0:
         new_image[i][j] = image[i][j]
         image[i][j] = 0
+        if min_max is not None:
+            min_max[0] = min(min_max[0], i)
+            min_max[1] = max(min_max[1], i)
+            min_max[2] = min(min_max[2], j)
+            min_max[3] = max(min_max[3], j)
         if 0 < i:
-            max_j = max(max_j, print_stone(image, i - 1, j, new_image))
+            max_j = max(max_j, print_stone(image, i - 1, j, new_image, boarder, min_max))
         if 0 < j:
-            max_j = max(max_j, print_stone(image, i, j - 1, new_image))
+            max_j = max(max_j, print_stone(image, i, j - 1, new_image, boarder, min_max))
         if i < image.shape[0] - 1:
-            max_j = max(max_j, print_stone(image, i + 1, j, new_image))
+            max_j = max(max_j, print_stone(image, i + 1, j, new_image, boarder, min_max))
         if j < image.shape[1] - 1:
-            max_j = max(max_j, print_stone(image, i, j + 1, new_image))
+            max_j = max(max_j, print_stone(image, i, j + 1, new_image, boarder, min_max))
+    else:
+        if boarder is not None:
+            boarder.append((i, j))
     return max_j
 
 
-def top_layer_stones(image, findTopStones=False):
+# to select the stones in the image and selectTopStones boolean to selecting the top stones
+def top_layer_stones(image, new_image=None, selectTopStones=False):
     dim = image.shape
-    new_image = creating_empty_image(dim, image.dtype)
-    i = (len(image) // 2) if findTopStones else 0
+    if new_image is None:
+        new_image = creating_empty_image(dim, image.dtype)
+    i = (len(image) // 2) if selectTopStones else 0
     j = 0
     if i > 0:
+        boarders = []
+        min_maxs = []
+        list_of_stones = []
         while j < dim[1]:
             if image[i][j] == 255:
-                j = print_stone(image, i, j, new_image)
+                boarder = []
+                min_max = [256, -1, 256, -1]
+                list_of_stones.append((i, j))
+                j = print_stone(image, i, j, new_image, boarder, min_max)
+                boarders.append(boarder)
+                min_maxs.append(min_max)
             j += 1
+        return list_of_stones, boarders, min_maxs, new_image
     else:
         while j < dim[1]:
-            i = 0
-            while i < dim[0]:
+            for i in range(dim[0]):
                 if image[i][j] == 255:
-                    j = print_stone(image, i, j, new_image)
+                    j = print_stone(image, i, j, new_image, None, None)
                     break
-                i += 1
             j += 1
-    return new_image
+        return new_image
 
 
+# returns each stones embedment in a list
+def embedment(image, location_of_stones):
+    list_of_embedment = []
+    l = len(list_of_embedment)
+    for xmin, xmax, ymin, ymax in location_of_stones:
+        for i in range(xmin, xmax + 1):
+            for j in range(ymin, ymax + 1):
+                if 0 < image[i][j] < 255:
+                    list_of_embedment.append((ymax - j) / (ymax - ymin))
+                    break
+            if l != len(list_of_embedment):
+                break
+    return list_of_embedment
+
+
+# to write the image
 def write_image(filename, image):
     if filename[-4:] != ".png":
         filename += ".png"
     if cv2.imwrite(os.getcwd() + "/" + filename, image):
         print(filename, "written successful")
     else:
-        print(filename, " written failure")
+        print(filename, " written failured")
 
 
+# function waiting for to close all the tabs opened
 def end_program():
     cv2.waitKey(0)
     cv2.destroyAllWindows()
@@ -104,21 +146,44 @@ def main():
     img = read_image("img1.png")
     img = image_scaling(img)
     show_image("image", img)
+
+    # to process the black line around the stones
+    stone_holding_part = image_conversion(img, cv2.COLOR_BGR2GRAY)
+    show_image("temp1", stone_holding_part)
+
+    dim = stone_holding_part.shape
+    for i in range(dim[0]):
+        for j in range(dim[1]):
+            stone_holding_part[i][j] = 123 if stone_holding_part[i][j] < 30 else 0
+    stone_holding_part = image_sharpness(stone_holding_part)
+    show_image("temp", stone_holding_part)
+
+    # enhancing the image to capture stones
     img = image_conversion(img, cv2.COLOR_BGR2RGB)
     img = image_contrast_brightness(img)
     img = image_sharpness(img)
     img = image_conversion(img, cv2.COLOR_BGR2GRAY)
+    show_image("black", img)
+
+    # converting the image into black and white based on a threshold
     dim = img.shape
     image_bw = creating_empty_image(dim, img.dtype)
+
     # setting threshold for the black and white image
+    threshold = 25
     for i in range(dim[0]):
         for j in range(dim[1]):
-            image_bw[i][j] = 255 if img[i][j] > 25 else 0
+            image_bw[i][j] = 255 if img[i][j] > threshold else 0
 
+    # separating the top layer of stones
     all_solid_stones = top_layer_stones(image_bw)
-    top_layer = top_layer_stones(all_solid_stones, True)
+    stones_coordinates, boarders, min_maxs, top_layer = top_layer_stones(all_solid_stones, stone_holding_part, True)
+    stones_embedment_percentage = embedment(top_layer, min_maxs)
 
+    print(stones_embedment_percentage)
     show_image("result", top_layer)
+
+    # writing the processed image to the file
     write_image("result", top_layer)
     end_program()
 

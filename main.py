d089793978ef0c4cacf69d23018eda11a1fac8fc
Bhanu Prasanth Konda <32713350+bhanuprasanthkonda@users.noreply.github.com>
2022-08-10 08:08:22 -0700

    Added GUI elements

--- main.py
+++ main.py
@@ -5,10 +5,14 @@ import os
 from tkinter import *
 from tkinter import filedialog
 from PIL import ImageTk, Image
+import warnings
 
-sys.setrecursionlimit(1000000)
+warnings.filterwarnings('ignore', '.*', )
+
+sys.setrecursionlimit(100000000)
 
 Debug = False
+
 
 # reads the image from the filepath
 def read_image(filepath):
@@ -32,10 +36,10 @@ def creating_empty_image(dim, dtype):
 
 
 # setting the contras and brightness of the image
-def image_contrast_brightness(image):
+def image_contrast_brightness(image, alpha=2.95, beta=-10):
     new_image = creating_empty_image(image.shape, image.dtype)
-    alpha = 4
-    beta = -15
+    # alpha = 2.95
+    # beta = -10
     for y in range(image.shape[0]):
         for x in range(image.shape[1]):
             new_image[y, x, 0] = np.clip(alpha * image[y, x, 0] + beta, 0, 255)
@@ -44,11 +48,13 @@ def image_contrast_brightness(image):
 
 # scaling the image to (350,) or (0,2000) dimensions
 def image_scaling(image):
-    scale_percent = min(35000 / image.shape[0], 200000 / image.shape[1])
+    scale_percent = min(23800 / image.shape[0], 200000 / image.shape[1])
+    # scale_percent = 23800 / image.shape[0]
+    # scale_percent = 200000 / image.shape[1]
     width = int(image.shape[1] * scale_percent / 100)
     height = int(image.shape[0] * scale_percent / 100)
     dim = (width, height)
-    return cv2.resize(image, dim, interpolation=cv2.INTER_AREA)
+    return cv2.resize(image, dim, fx=3, fy=4, interpolation=cv2.INTER_AREA)
 
 
 # sharpening the image
@@ -56,7 +62,7 @@ def image_sharpness(image):
     kernel = np.array([[0, -1, 0],
                        [-1, 5, -1],
                        [0, -1, 0]])
-    image_sharp = cv2.filter2D(src=image, ddepth=-1, kernel=kernel)
+    image_sharp = cv2.filter2D(src=image, ddepth=-2, kernel=kernel)
     return image_sharp
 
 
@@ -145,7 +151,7 @@ def top_layer_stones(image, new_image=None, selectTopStones=False):
 
 
 def overlap(img, toplayer):
-    print(img.shape, toplayer.shape)
+    # print(img.shape, toplayer.shape)
     dim = img.shape
     for i in range(dim[0]):
         for j in range(dim[1]):
@@ -153,6 +159,7 @@ def overlap(img, toplayer):
                 img[i][j] = [255, 255, 255]
     show_image("overlap", img)
     write_image("overlap", img)
+    return img
 
 
 # returns each stones embedment in a list
@@ -191,9 +198,10 @@ def write_on_image(image, pos, text):
 def write_image(filename, image):
     if filename[-4:] != ".png":
         filename += ".png"
-    if cv2.imwrite(os.getcwd() + "/" + filename, image):
+    wr = cv2.imwrite(os.getcwd() + "/" + filename, image)
+    if Debug and wr:
         print(filename, "written successful")
-    else:
+    elif Debug:
         print(filename, " written failed")
 
 
@@ -204,21 +212,38 @@ def end_program():
         cv2.destroyAllWindows()
 
 
-def main(img=None):
+def main(threshold=20.0, alpha=2.95, beta=-10.0, img="", shouldBreak=False):
     # print(img)
     if img is None:
         img = "img1.png"
     img = read_image(img)
+    # print(img[0][0])
     img = image_scaling(img)
     img_copy = img.copy()
+    # bm, gm, rm = float("inf"), float("inf"), float("inf")
+    for i in range(img.shape[0]):
+        for j in range(img.shape[1]):
+            b, g, r = img[i][j]
+            if r >= 66 and g >= 135 and b >= 155:
+                img[i][j] = [133, 57, 10]
+            elif r <= 35 and g <= 35 and b <= 35:
+                img[i][j] = [0, 0, 0]
+    #         elif i == 0:
+    #             bm = min(bm, b)
+    #             gm = min(gm, g)
+    #             rm = min(rm, r)
+    # print(bm, gm, rm)
     show_image("image", img)
     # print(img.shape)
+    # end_program()
+    # return
 
     # to process the black line around the stones
     grey_scale_img = image_conversion(img, cv2.COLOR_BGR2GRAY)
     show_image("grey_scale_img", grey_scale_img)
     # progress("*" * 2 + " 10% Complete")
-
+    # end_program()
+    # return
     dim = grey_scale_img.shape
     for i in range(dim[0]):
         for j in range(dim[1]):
@@ -226,14 +251,18 @@ def main(img=None):
     stone_holding_part = image_sharpness(grey_scale_img)
     stone_holding_part = remove_tiny_parts(stone_holding_part)
     show_image("stone_holding_part", stone_holding_part)
+    # end_program()
+    # return
     # progress("**" * 2 + " 20% Complete")
 
     # enhancing the image to capture stones
     img = image_conversion(img, cv2.COLOR_BGR2RGB)
-    img = image_contrast_brightness(img)
+    img = image_contrast_brightness(img, alpha, beta)
     img = image_sharpness(img)
     img = image_conversion(img, cv2.COLOR_BGR2GRAY)
     show_image("black", img)
+    # end_program()
+    # return
     # progress("***" * 2 + " 30% Complete")
 
     # converting the image into black and white based on a threshold
@@ -241,22 +270,30 @@ def main(img=None):
     image_bw = creating_empty_image(dim, img.dtype)
 
     # setting threshold for the black and white image
-    threshold = 25
+    # threshold = 23
+    # print("threshold",threshold)
     for i in range(dim[0]):
         for j in range(dim[1]):
             image_bw[i][j] = 255 if img[i][j] > threshold else 0
-
     show_image("image_bw", image_bw)
+    end_program()
+    if shouldBreak:
+        write_image("image_bw", image_bw)
+        # print("complete")
+        return image_bw
+
     # progress("****" * 2 + " 40% Complete")
 
     # separating the top layer of stones
     all_solid_stones = top_layer_stones(image_bw)
     show_image("all_solid_stones", all_solid_stones)
+    # end_program()
+    # return
     # progress("*****" * 2 + " 50% Complete")
     stones_coordinates, boarders, min_maxes, top_layer = top_layer_stones(all_solid_stones, stone_holding_part, True)
     # progress("*******" * 2 + " 70% Complete")
     stones_embedment_percentage, lst = embedment(top_layer, min_maxes)
-    overlap(img_copy, top_layer)
+    overlap_img = overlap(img_copy, top_layer).copy()
     # progress("********" * 2 + " 80% Complete")
     top_layer = image_conversion(top_layer, cv2.COLOR_GRAY2RGB)
     # progress("*********" * 2 + " 90% Complete")
@@ -270,15 +307,17 @@ def main(img=None):
         y //= len(boarders[i])
         j *= 100
         top_layer = write_on_image(top_layer, (y - 20, x), str(j)[:5] + "%")
-        print(i + 1, ":", j, "%")
+        overlap_img = write_on_image(overlap_img, (y - 20, x), str(j)[:5] + "%")
+        # print(i + 1, ":", j, "%")
         report.append(str(i + 1) + " : " + str(j)[:5] + "%")
     show_image("result", top_layer)
     # progress("**********" * 2 + " 100% Complete")
 
     # writing the processed image to the file
     write_image("result", top_layer)
+    write_image("overlap", overlap_img)
     end_program()
-    return top_layer, report
+    return overlap_img, report
 
 
 # def dummy_photo():
@@ -321,6 +360,8 @@ def main(img=None):
 
 def GUI():
     root = Tk()
+    res = None
+    label2 = None
     try:
         root.title("Image_Processing")
         root.geometry("775x700")
@@ -332,10 +373,12 @@ def GUI():
         Label(root, text="                                     ", justify="center").grid(row=1, column=0)
 
         L1 = Label(root, textvariable=text, justify="center")
-        L1.grid(row=1, column=1)
+        L1.grid(row=1, columnspan=5)
         # root.grid_rowconfigure(1, weight=1)
-        root.grid_columnconfigure(1, weight=1) # making the input to the center
+        root.grid_columnconfigure(1, weight=1)  # making the input to the center
         label1 = None
+        threshold = 23
+        inptext = None
 
         def open_file():
             global label1
@@ -351,7 +394,7 @@ def GUI():
                 image1 = image1.resize(
                     (int(image1.width * scale_percent * 0.35), int(0.35 * image1.height * scale_percent)),
                     Image.ANTIALIAS)
-                print(image1.width, image1.height)
+                # print(image1.width, image1.height)
                 test = ImageTk.PhotoImage(image1)
                 try:
                     label1.configure(image=test)
@@ -359,7 +402,7 @@ def GUI():
                 except:
                     label1 = Label(image=test, justify="center")
                     label1.image = test
-                label1.grid(row=3, column=0, columnspan=3)
+                label1.grid(row=3, column=0, columnspan=5)
 
             def resultImg():
                 global Debug
@@ -372,28 +415,77 @@ def GUI():
             # def GUI_ProgressBar(text):
             #     return
             #     Label(root, text=text, font='Aerial 12', justify="left").grid(row=6, column=0)
-
             def Analyze():
+                global res
+                global label2
                 global final_img
-                final_img, report = main(filepath)
-                f_img = Image.open("result.png")
+                threshold = float(inptext_threshold.get().strip())
+                # print(threshold)
+                alpha = float(inptext_intensity.get().strip())
+                beta = float(inptext_reduction.get().strip())
+                final_img, report = main(threshold, alpha, beta, filepath, False)
+                f_img = Image.open("overlap.png")
                 scale_percent = 2000 / f_img.width
                 image2 = f_img.resize(
                     (int(f_img.width * scale_percent * 0.35), int(0.35 * f_img.height * scale_percent)),
                     Image.ANTIALIAS)
                 f_test = ImageTk.PhotoImage(image2)
                 try:
+                    label2.destroy()
+                    label2 = Label(image=f_test, justify="center")
                     label2.configure(image=f_test)
                     label2.image = f_test
                 except:
                     label2 = Label(image=f_test, justify="center")
                     label2.image = f_test
-                label2.grid(row=8, column=0, columnspan=3)
+                label2.grid(row=12, column=0, columnspan=5)
                 stringreport = "\n".join(report)
                 string = StringVar()
                 string.set(stringreport)
-                Label(root, textvariable=string, font='Aerial 12', justify="left").grid(row=9, column=1)
-                Button(root, text="Result", command=resultImg).grid(row=6, column=1)
+                try:
+                    res.destroy()
+                except:
+                    pass
+                res = Label(root, textvariable=string, font='Aerial 12', justify="left")
+                res.grid(row=13, column=1)
+                root.grid_columnconfigure(13, weight=1)
+                Button(root, text="Result", command=resultImg).grid(row=11, column=1)
+
+            def setThreshold():
+                global res
+                global label2
+                global final_img
+                threshold = float(inptext_threshold.get().strip())
+                # print(threshold)
+                alpha = float(inptext_intensity.get().strip())
+                beta = float(inptext_reduction.get().strip())
+                f_img = main(threshold, alpha, beta, filepath, True)
+                f_img = Image.open("image_bw.png")
+                scale_percent = 2000 / f_img.width
+                image2 = f_img.resize(
+                    (int(f_img.width * scale_percent * 0.35), int(0.35 * f_img.height * scale_percent)),
+                    Image.ANTIALIAS)
+                f_test = ImageTk.PhotoImage(image2)
+                try:
+                    label2.destroy()
+                    label2 = Label(image=f_test, justify="center")
+                    label2.configure(image=f_test)
+                    label2.image = f_test
+                except:
+                    label2 = Label(image=f_test, justify="center")
+                    label2.image = f_test
+                label2.grid(row=9, column=0, columnspan=5)
+                # stringreport = "\n".join(report)
+                # string = StringVar()
+                # string.set(stringreport)
+                # try:
+                #     res.destroy()
+                # except:
+                #     pass
+                # res = Label(root, textvariable=string, font='Aerial 12', justify="left")
+                # res.grid(row=9, column=1)
+                # root.grid_columnconfigure(11, weight=1)
+                # Button(root, text="Result", command=resultImg).grid(row=6, column=1)
 
             def Preview():
                 global Debug
@@ -405,7 +497,29 @@ def GUI():
 
             if filepath:
                 Button(root, text="Preview", command=Preview).grid(row=4, column=1)
-                Button(root, text="Analyse", command=Analyze).grid(row=5, column=1)
+                Button(root, text="Start Analyse", command=Analyze).grid(row=10, column=1)
+                Button(root, text="Load Image", command=setThreshold).grid(row=8, column=1)
+
+                Label(root, text="Threshold:").grid(row=5, column=1)
+                # inptext_threshold = StringVar()
+                inptext_threshold = Entry(root)
+                inptext_threshold.grid(row=5, column=2)
+                inptext_threshold.focus()
+                inptext_threshold.insert(0, "20")
+
+                Label(root, text="Intensity:").grid(row=6, column=1)
+                # inptext_text = StringVar()
+                inptext_intensity = Entry(root)
+                inptext_intensity.grid(row=6, column=2)
+                inptext_intensity.focus()
+                inptext_intensity.insert(0, "2.95")
+
+                Label(root, text="Reduction:").grid(row=7, column=1)
+                # inptext_ = StringVar()
+                inptext_reduction = Entry(root)
+                inptext_reduction.grid(row=7, column=2)
+                inptext_reduction.focus()
+                inptext_reduction.insert(0, "-10")
 
         Button(root, text="Browse", command=open_file).grid(row=2, column=1)
 
@@ -413,7 +527,8 @@ def GUI():
     except:
         pass
     finally:
-        root.destroy()
+        # root.destroy()
+        sys.exit()
 
 
 if __name__ == "__main__":
@@ -421,4 +536,4 @@ if __name__ == "__main__":
     # write_image("img2.png", img)
     # show_image("dummy", img)
     GUI()
-    # main()
+    # main("IMG_6228.png")
